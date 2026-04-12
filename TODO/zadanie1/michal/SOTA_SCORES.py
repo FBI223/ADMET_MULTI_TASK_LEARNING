e8dac7e37cfa2584f7e954037298e358d573d313
kopcz <kopczewski.michal101@gmail.com>
2026-04-12 16:07:43 +0200

    Added SOTA to comparison table and saved it as image

---
+++ TODO/zadanie1/michal/SOTA_SCORES.py
@@ -0,0 +1,17 @@
+#Data from Quantum Enhanced MTL paper, AUC scores
+
+SOTA_SCORES = {
+    'bioavailability_ma': 0.748,
+    'hia_hou': 0.989,
+    'pgp_broccatelli': 0.938,
+    'bbb_martins': 0.916,
+    'cyp2c9_veith': 0.859,
+    'cyp2d6_veith': 0.790,
+    'cyp3a4_veith': 0.916,
+    'cyp2c9_substrate_carbonmangels': 0.441,
+    'cyp2d6_substrate_carbonmangels': 0.736,
+    'cyp3a4_substrate_carbonmangels': 0.662,
+    'hERG_Karim': 0.880,
+    'ames': 0.918,
+    'dili': 0.878
+}
